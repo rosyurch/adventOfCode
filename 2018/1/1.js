5f6e42a0adb9e34f163020c7d45f4f1e4929138e
Rostyslav Yurchyk <ros_ur@ukr.net>
2019-10-31 22:43:13 +0100

    day 1 part 1

--- 2018/1/1.js
+++ 2018/1/1.js
@@ -1,7 +1,42 @@
 const data = require('./input.js');
 
-const input = data.split('\n');
+const resultPart1 = data
+    .split('\n')
+    .map(num => Number(num))
+    .reduce((acc, cur) => acc + cur);
 
-const result = input.map(num => Number(num)).reduce((acc, cur) => acc + cur);
+// console.log(resultPart1);
 
-console.log(result);
+// part 1 - sum of all 'frequencies'
+// 466 - correct
+
+const resultPart2 = data => {
+    // const set = new Set();
+    const hash = {};
+    // let sum = 0;
+    let firstRepeated;
+
+    data.split('\n')
+        .map(num => Number(num))
+        .reduce((acc, cur) => {});
+
+    // data.split('\n')
+    //     .map(num => {
+    //         num = Number(num);
+    //         sum += num;
+    //         set.add(sum);
+
+    //         return num;
+    //     })
+    //     .reduce((acc, cur) => {
+    //         if (set.has(acc + cur)) firstRepeated = acc + cur;
+    //         // console.log(set.has(res));
+
+    //         return acc + cur;
+    //     });
+
+    // console.log(set);
+    return firstRepeated;
+};
+
+console.log(resultPart2(data));
